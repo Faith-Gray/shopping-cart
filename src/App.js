f01c3347bff8ae5867b06dcce2daa33651cfeba4
Faith <fhgray96@gmail.com>
2022-09-15 13:45:35 -0500

    add links for home and shop page

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { Route, Routes } from 'react-router-dom';
+import { Link, Route, Routes } from 'react-router-dom';
 import './App.css';
 import Home from './Components/Home';
 import Shop from './Components/Shop';
@@ -6,6 +6,18 @@ import Shop from './Components/Shop';
 function App() {
   return (
     <div className="App">
+      <nav>
+        <ul>
+          <li>
+            <Link to="/">Home</Link>
+          </li>
+          <li>
+            <Link to="/shop">Shop</Link>
+          </li>
+        </ul>
+      </nav>
+
+
       <Routes>
       <Route path="/" element={<Home />} />
       <Route path="/shop" element={<Shop />} />

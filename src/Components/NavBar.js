4b6a7776e9022601cc68dc2331e98c00bfe7c76a
Faith <fhgray96@gmail.com>
2022-09-18 17:14:23 -0500

    update navbar styling. Update navbar case

--- src/Components/NavBar.js
+++ src/Components/NavBar.js
@@ -1,4 +1,4 @@
-import { Link, Route, Routes } from 'react-router-dom';
+import { Link } from 'react-router-dom';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faPaw } from '@fortawesome/free-solid-svg-icons';
 import { faMagnifyingGlass } from '@fortawesome/free-solid-svg-icons';
@@ -6,46 +6,46 @@ import { faCartShopping } from '@fortawesome/free-solid-svg-icons';
 
 function NavBar() {
     return (
-      <div className="navBar">
-        <nav>
-          <div className='nav_logoContainer'>
-            <div className='nav_logo'>
-              <FontAwesomeIcon icon={faPaw} />
-            </div>
-            <div className='nav_title'>DogRun</div>
-          </div>
-  
-          <ul>
-            <li>
-              <Link to="/">Home</Link>
-            </li>
-            <li>
-              <Link to="/shop">Shop All</Link>
-            </li>
-            <li>
-              <Link to="/wear">Wear</Link>
-            </li>
-            <li>
-              <Link to="/walk">Walk</Link>
-            </li>
-            <li>
-              <Link to="/chains">Chains</Link>
-            </li>
-            <li>
-              <Link to="/reviews">Reviews</Link>
-            </li>
-          </ul>
-          <div className='nav_search'>
-            <FontAwesomeIcon icon={faMagnifyingGlass} />
-          </div>
+      
+      <nav className='navBar'>
+        <div className='nav_logoContainer'>
           <div className='nav_logo'>
-            <FontAwesomeIcon icon={faCartShopping} />
+            <FontAwesomeIcon icon={faPaw} />
           </div>
-        </nav>
+          <div className='nav_title'>DogRun</div>
+        </div>
+
+        <ul>
+          <li>
+            <Link to="/">HOME</Link>
+          </li>
+          <li>
+            <Link to="/shop">SHOP ALL</Link>
+          </li>
+          <li>
+            <Link to="/wear">WEAR</Link>
+          </li>
+          <li>
+            <Link to="/walk">WALK</Link>
+          </li>
+          <li>
+            <Link to="/chains">CHAINS</Link>
+          </li>
+          <li>
+            <Link to="/reviews">REVEIWS</Link>
+          </li>
+        </ul>
+        <div className='nav_search'>
+          <FontAwesomeIcon icon={faMagnifyingGlass} />
+        </div>
+        <div className='nav_cart'>
+          <FontAwesomeIcon icon={faCartShopping} />
+        </div>
+      </nav>
   
   
 
-      </div>
+      
     );
   }
   

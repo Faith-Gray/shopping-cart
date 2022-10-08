1633aefae73f14ba5eb5df7e8756c5848e9d3693
Faith <fhgray96@gmail.com>
2022-10-07 22:29:12 -0500

    Add title to shop page

--- src/Components/Shop.js
+++ src/Components/Shop.js
@@ -3,6 +3,9 @@ import SortBar from "./PageComponents/SortBar";
 function Shop() {
     return (
       <div className="shop">
+        <header className="pageTitle">
+          SHOP ALL
+        </header>
         <SortBar />
         This is the Shop Page
       </div>

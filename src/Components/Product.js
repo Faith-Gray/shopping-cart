7a0d12658208b998b0c9215d97d10039825eb8f9
Faith <fhgray96@gmail.com>
2022-10-12 20:54:24 -0500

    Add product

---
+++ src/Components/Product.js
@@ -0,0 +1,14 @@
+
+
+const Product = ({ product }) => {
+    return (
+        <div className="productCard">
+            {/* make image and the product name a link to the product page */}
+            <div className="productImage1"></div>
+            <div className="productImage2"></div>
+            <div className="productName"></div>
+            <div className="productReview"></div>
+            <div className="productPrice"></div>
+        </div>
+    )
+}
--- src/Components/Products.js
+++ src/Components/Products.js
@@ -1,14 +1,21 @@
+import Product from './Product';
 
+const products = [
+    {id: 1, productName:'shoes', productReview: '5 stars', price:'$20'},
+    {id: 2, productName:'hat', productReview: '5 stars', price:'$25'}    
+];
 
 const Products = () => {
-    <main>
-        {products.map((product) => (
-            <div>
-                key={product.id} 
-                <Product product={product}
-            </div>
-        ))}
-    </main>
+    return (
+        <div>
+            {products.map((product) => (
+                <div>
+                    <Product product={product}
+                </div>
+            ))}
+        </div>        
+    )
+
 }
 
 

1d36fb69b89b2d95366d43d0564a3bc8da517ca8
Faith <fhgray96@gmail.com>
2023-02-20 00:14:50 -0600

    Add image carousel functionality

--- src/Components/ProductView/ProductView.js
+++ src/Components/ProductView/ProductView.js
@@ -10,6 +10,7 @@ import { stripHtml } from "string-strip-html";
 const ProductView = ({ onAddToCart }) => {
     const [product, setProduct] = useState({});
     const [pageLoading, setPageLoading] = useState(true);
+    const [productImageNumber, setProductImageNumber] = useState(0);
     
     const handleAddToCart = () => {
       onAddToCart(product.id, 1);
@@ -24,6 +25,7 @@ const ProductView = ({ onAddToCart }) => {
 
         const { result } = stripHtml(description);
 
+        // const [productPreviewImage, setProductPreviewImage] = useState(image?.url);
 
         setProduct({
             id,
@@ -47,6 +49,12 @@ const ProductView = ({ onAddToCart }) => {
         fetchProduct(id[2]);
     }, []);
 
+    const handleClick = (event, key) => {
+      console.log(event.target);
+      console.log('key index: ', key)
+      setProductImageNumber(key);
+    };
+
     if (pageLoading) return <Loading />;
     return (
       <div className="Product View">
@@ -54,10 +62,10 @@ const ProductView = ({ onAddToCart }) => {
 
         <div className="pageContainer">
           <div className="productImageContainer">
-          <img className="product__image" src={product.src} alt={product.name}/>
+          <img className="product__image" src={product.assets[productImageNumber].url} alt={product.name}/>
 
             <div className="productImageCarousel"> 
-              {product.assets.map((product) => <img src={product.url} alt ='wft' key={product.url}/> )}
+              {product.assets.map((product, key) => <img src={product.url} alt ='product images' key={product.url} onClick={event => handleClick(event, key)}/> )}
             </div>
           </div>
           <div className="productDescriptionContainer">
